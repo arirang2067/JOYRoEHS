6de67dac88a289a0d2c06d7b3fa09ac4e458b2d5
dmitri <arirang2067@naver.com>
2019-06-07 20:33:46 +0900

    0607

--- test_joystic/src/joy_test.cpp
+++ test_joystic/src/joy_test.cpp
@@ -1,22 +1,22 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <sensor_msgs/Joy.h>
-
+#include <time.h>
 
 class TeleopTurtle
 {
 public:
   TeleopTurtle();
+  std_msgs::String string;
+  ros::Publisher vel_pub_;
 
 private:
   void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
-
   ros::NodeHandle nh_;
-
+  
   int linear_, angular_;
-  ros::Publisher vel_pub_;
+  
   ros::Subscriber joy_sub_;
-
 };
 
 
@@ -24,18 +24,17 @@ TeleopTurtle::TeleopTurtle():
   linear_(1),
   angular_(2)
 {
+  ROS_INFO("test_joystic_start");
   nh_.param("axis_linear", linear_, linear_);
   nh_.param("axis_angular", angular_, angular_);
-  
   vel_pub_ = nh_.advertise<std_msgs::String>("/heroehs/test_joystic", 10);
-
-
   joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopTurtle::joyCallback, this);
 }
 
 void TeleopTurtle::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
-  std_msgs::String string;
+  
+  ROS_INFO("time");
   if(joy->axes[6] == 1)
     string.data = "left";
   else if(joy->axes[6] == -1)
@@ -51,8 +50,8 @@ void TeleopTurtle::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
   else if(joy->buttons[2] == 1)
     string.data = "stop";
   else
-    string.data = "Default";
-  vel_pub_.publish(string.data);
+    string.data = "stop";
+  //vel_pub_.publish(this->string.data);
 }
 
 
@@ -60,7 +59,20 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "teleop_turtle");
   TeleopTurtle teleop_turtle;
-
-  ros::spin();
+  clock_t start, current;
+  start = clock();
+  
+  while(ros::ok())
+  {
+    if((float)(current-start)/(CLOCKS_PER_SEC)>0.04)
+    {
+      ROS_INFO("time1");
+      teleop_turtle.vel_pub_.publish(teleop_turtle.string.data);
+      start = clock();   
+    }
+    else current = clock();
+    usleep(1000);
+    ros::spinOnce();
+  }
 }
 
